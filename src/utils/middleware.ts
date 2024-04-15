8c28ca7822adf9bfcf88d5c89327d5bdf9090aec
jumbishaara <153064398+jumbishaara@users.noreply.github.com>
2024-04-15 11:01:21 +0530

    middleware.ts

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,16 +1,16 @@
-import { useAuthStore } from '../store/authStore';
-import { Redirect, Routes } from 'react-router-dom';
-import React from 'react';
+// import { useAuthStore } from '../store/authStore';
+// import { Redirect, Routes } from 'react-router-dom';
+// import React from 'react';
 
-export const ProtectedRoute: React.FC<any> = ({ component: Component, ...rest }) => {
-  const authStore = useAuthStore();
+// export const ProtectedRoute: React.FC<any> = ({ component: Component, ...rest }) => {
+//   const authStore = useAuthStore();
 
-  return (
-    <Routes>
-      {...rest}
-      render={(props) => {
-        return authStore.isAuthenticated ? <Component {...props} /> : <Redirect to="/signin" />;
-      }
-    </Routes>
-  );
-};
+//   return (
+//     <Routes>
+//       {...rest}
+//       render={(props) => {
+//         return authStore.isAuthenticated ? <Component {...props} /> : <Redirect to="/signin" />;
+//       }
+//     </Routes>
+//   );
+// };
